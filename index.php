78334dbe7220342d32b00fbc5aca4d8cf84cf3c1
itsjackie00 <jackiedanso33@gmail.com>
2024-05-13 20:17:05 +0200

    included view header

--- index.php
+++ index.php
@@ -1,11 +1,2 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Php-oop-1</title>
-</head>
-<body>
-    
-</body>
-</html>
+<?php
+include __DIR__ . '/View/header.php';
