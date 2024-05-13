943a97123a27479b8b2c9c42879fedd3f5c86a4b
itsjackie00 <jackiedanso33@gmail.com>
2024-05-13 20:23:43 +0200

    added footer content

---
+++ View/footer.php
@@ -0,0 +1,10 @@
+<footer>
+    <div class="container">
+        <p class="text-center">
+        Made with &hearts; by Jacqueline Wuaku
+        </p>
+    </div>
+</footer>
+</div>
+</body>
+</html>
